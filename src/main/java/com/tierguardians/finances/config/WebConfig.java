afb6a3a4f79c87a846339c65a25f53e88068137e
ddddabi <ii887742@gmail.com>
2025-07-21 17:44:19 +0900

    fix: ip 수정

--- src/main/java/com/tierguardians/finances/config/WebConfig.java
+++ src/main/java/com/tierguardians/finances/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        //"http://localhost:3000",
-                        "http://192.168.0.55:3000"
+                        "http://192.168.56.1:3000"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
