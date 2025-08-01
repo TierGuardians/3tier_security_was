b582493f520bf23c10f4d83c124630c80a33bd7e
ddddabi <ii887742@gmail.com>
2025-08-01 11:02:26 +0900

    chore: 사용 안 하는 클래스 삭제

--- src/main/java/com/tierguardians/finances/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tierguardians.finances.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
